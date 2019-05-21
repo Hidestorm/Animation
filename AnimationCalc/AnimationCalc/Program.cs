777628d1af0fec90f7d480e142fd79643bd9a053
xuj-n <xuj-n@glodon.com>
2019-05-21 18:11:14 +0800

    添加文件 插入排序

---
+++ AnimationCalc/AnimationCalc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sort;
+
+namespace AnimationCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] d = { 2, 3, 1, 9, 8, 4, 10 };
+            List<double> listd = new List<double>();
+            
+            foreach(double j in d)
+            {
+                listd.Add(j);
+            }
+
+            sortMethod sortTest = new InsertSort();
+            sortTest.sort(ref listd);
+
+            foreach(double num in listd)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AnimationCalc/Sort/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    public interface sortMethod
+    {
+        void sort(ref List<double> list);
+    }
+
+    public class InsertSort : sortMethod
+    {
+        public void sort(ref List<double> list)
+        {
+            // 从下标为1的元素开始选择合适的位置插入，因为下标为0的只有一个元素，默认是有序的
+            for (int i = 1; i < list.Count(); i++)
+            {
+                double tmp = list[i];
+                int j = i;
+                while (j > 0 && tmp < list[j - 1])
+                {
+                    list[j] = list[j - 1];
+                    j--;
+                }
+
+                // 存在比其小的数，插入
+                if (j != i)
+                {
+                    list[j] = tmp;
+                }
+            }
+        }
+    }
+}
